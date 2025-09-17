d3436a954496b232ac3cb0368ff9659d7797f4c8
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-09-17 21:11:31 +0000

    Gerando dados (sem criar o arquivo JSON) + makefile e scrips incompletos

---
+++ makefile
@@ -0,0 +1,60 @@
+
+#variaveis
+
+PRODUCER_DIR = sensor
+
+#TODO: ainda nao temos tudo para rodar tudo
+all:
+
+#TODO: falta parar e excluir o consumer
+stop:
+	@echo "Parando containers..."
+	docker ps -a
+	docker stop kafka1 || true
+	docker stop kafka2 || true
+	docker stop kafka3 || true
+	docker stop sensor || true
+	docker rm sensor || true
+	docker rm kafka1 || true
+	docker rm kafka2 || true
+	docker rm kafka3 || true
+	docker ps -a
+
+# Constrói as imagens
+build:
+	@echo "Criando os containers..."
+	docker-compose build
+
+# Sobe os containers
+up:
+	@echo "Subindo os containers..."
+	docker-compose up -d
+
+# Lista os containers
+status:
+	@echo "Status dos containers:"
+	docker ps -a
+
+# Mostra logs em tempo real
+logs:
+	@echo "Mostrando logs dos containers (Ctrl+C para sair)..."
+	docker-compose logs -f
+
+# Logs separados
+logs-producer:
+	@echo "Mostrando logs do Producer..."
+	docker-compose logs -f sensor
+
+logs-kafka:
+	@echo "Mostrando logs do Kafka..."
+	docker-compose logs -f kafka
+
+# Sobe o frontend
+frontend:
+	@echo "Iniciando frontend..."
+
+# Limpa build do produtor e consumidor -> ta sem o consumidor
+clean:
+	stop
+	@echo "Limpando builds..."
+	docker ps -a
--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,5 +1,6 @@
-"""Gerar dados periodicos (Json)
-    Uma fábrica inteligente possui diversas máquinas espalhadas em diferentes setores (linha de produção,
+"""
+Gerar dados periodicos (formato Json)
+Uma fábrica inteligente possui diversas máquinas espalhadas em diferentes setores (linha de produção,
 refrigeração, empacotamento, etc.). Cada máquina é equipada com sensores que enviam dados (temperatura,
 vibração, consumo de energia, etc.) continuamente para um sistema central que processa esses dados em tempo
 real para detectar anomalias, falhas ou padrões de uso.
@@ -11,13 +12,14 @@ import random
 import time
 from datetime import datetime
 from kafka import KafkaProducer,KafkaAdminClient
-from kafka.structs import TopicPartition
 
 sys.stdout.reconfigure(line_buffering=True)
 
 # Configurações da simulação
+#TODO : ver se tem mais setores
 SETORES = ["linha_producao", "refrigeracao", "empacotamento"]
 
+#TODO : ver se o min e max fazem sentido
 TIPOS_SENSORES = {
     "temperatura": {"min": 20.0, "max": 80.0, "unidade": "C"},
     "vibracao": {"min": 0.5, "max": 5.0, "unidade": "mm/s"},
@@ -25,7 +27,6 @@ TIPOS_SENSORES = {
 }
 
 #dados-sensores sera o nome do topico kafka que o sensor vai enviar as mensagens
-NOME_ARQUIVO = "dados-sensores.json"
 KAFKA_BROKERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka1:9092").split(",")
 KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'dados-sensores')
 
@@ -50,24 +51,6 @@ def gerar_dados_maquina():
 
     return dados_maquina
 
-def gerar_arquivos_json(num_arquivos=1):
-    """
-    Gera um ou mais arquivos JSON com dados simulados.
-    Cada arquivo contém dados de um número aleatório de máquinas.
-    """
-    for i in range(num_arquivos):
-        # Gera um número aleatório de registros para o arquivo
-        num_registros = random.randint(10, 20)
-        dados_coletados = [gerar_dados_maquina() for _ in range(num_registros)]
-
-        try:
-            with open(NOME_ARQUIVO, "w") as f:
-                json.dump(dados_coletados, f, indent=4)
-            print(f"Arquivo '{NOME_ARQUIVO}' gerado com sucesso!")
-
-        except IOError as e:
-            print(f"Erro ao escrever o arquivo: {e}")
-
 def criar_producer(retries=10, delay=5):
     """"
     Cria um KafkaProducer com tentativas de reconexão.
@@ -83,9 +66,11 @@ def criar_producer(retries=10, delay=5):
             )
             print(f"Conectado ao Kafka! Brokers tentados: {KAFKA_BROKERS}", flush=True)
             return producer
+
         except Exception as e:
             print(f"Tentativa {i+1}/{retries} falhou: {e}", flush=True)
             time.sleep(delay)
+
     raise Exception("Não foi possível conectar ao Kafka.")
 
 def obter_lider(topico, particao):
@@ -100,37 +85,32 @@ def obter_lider(topico, particao):
                         broker = next(b for b in t["partitions"] if b["partition"] == particao)
                         return f"brokerId={leader}"
         return "desconhecido (sem líder definido)"
+
     except Exception as e:
         return f"desconhecido ({e})"
-    
+
 if __name__ == "__main__":
     # Para simular a geração contínua
     while True:
-        gerar_arquivos_json(num_arquivos=1)
+
+        # Gera um número aleatório de registros para o arquivo
+        num_registros = random.randint(10, 20)
+        dados_coletados = [gerar_dados_maquina() for _ in range(num_registros)]
+
         #manda mensagem ao kafka
-
         producer = criar_producer()
-
-        with open(NOME_ARQUIVO, "r") as f:
-            dados_coletados = json.load(f)
 
         for dados in dados_coletados:
             future = producer.send(KAFKA_TOPIC, json.dumps(dados).encode('utf-8'))
             result = future.get(timeout=10)
-            lider = obter_lider(result.topic, result.partition) 
+            lider = obter_lider(result.topic, result.partition)
             print(
                 f"Mensagem enviada para {result.topic} [partição {result.partition}, offset {result.offset}] "
                 f"no broker líder {lider}",
                 flush=True
             )
 
+        producer.flush()
         producer.close()
-
-        # Exclui o arquivo após enviar todas as informações ao Kafka
-        try:
-            os.remove(NOME_ARQUIVO)
-            print(f"Arquivo '{NOME_ARQUIVO}' excluído com sucesso!")
-        except OSError as e:
-            print(f"Erro ao excluir o arquivo: {e}")
 
         time.sleep(5)
